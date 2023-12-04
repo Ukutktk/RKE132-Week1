21566e05551d3fb7798651ede20d1de9b189a9d5
Uku <149318155+Ukutktk@users.noreply.github.com>
2023-12-04 16:31:14 +0200

    Add files via upload

---
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasjutajat nimepidi
+
+
+Console.WriteLine("Enter your name"); //Output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + userName); //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!"); //Output
